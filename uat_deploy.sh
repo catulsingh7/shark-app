2b5ac03f91ab6c08fd568e6335026c6f010b0402
root <root@localhost.localdomain>
2020-07-11 20:01:00 +0530

    update uat file

--- uat_deploy.sh
+++ uat_deploy.sh
@@ -1,9 +1,9 @@
 mkdir /backup
 
-sudo cd /opt/
-sudo zip -r /backup/backup-000778 .
+cd /opt/
+zip -r /backup/backup-000778 .
 
 
-sudo cd /opt
-sudo npm install 
-sudo nohup node app.js &
+cd /opt
+npm install 
+nohup node app.js &
